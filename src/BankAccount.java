521e3d6cfb4a402336c244a94d464516a3c94d02
zangkai233 <kevinzangofficial@gmail.com>
2026-09-21 19:09:46 -0400

    Create basic bank account system

---
+++ src/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+    private String owner;
+    private double balance;
+
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("Kai", 1000);
+
+        System.out.println("Current balance: $" + account.getBalance());
+    }
+}
